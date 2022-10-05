e328ccf17579f1df7b5e71f10ca298701952dc41
Gabriel Diniz <gabriel.assisdiniz@outlook.com>
2022-10-05 17:16:12 -0300

    Atividades 7 e 9

--- Lista 2/Atividade7/Program.cs
+++ Lista 2/Atividade7/Program.cs
@@ -3,6 +3,15 @@
 class Program{
 
     public static void Main(String[] args){
-        
+        int quant, a = 0, fib = 1;
+
+        Console.WriteLine("Insira a quantidade de casas que deseja ver da sequência de fibonacci: ");
+        quant = int.Parse(Console.ReadLine());
+
+        for(int i = 0; i < quant; i++){
+            Console.Write(a + " " + fib + " ");
+            a+=fib;
+            fib+=a;
+        }
     }
 }
