4762ec0ee437863b03fe2ba888c53ad6ef2dd203
Gabriel Diniz <gabriel.assisdiniz@outlook.com>
2022-10-05 16:20:20 -0300

    Atividade6

---
+++ Lista 2/Atividade6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program{
+
+    public static void Main(String[] args){
+
+        /*
+            Escreva um algoritmo que lê um valor n inteiro e positivo e que calcula a seguinte soma:
+            S = 1 + 1/2 + 1/3 + 1/4 + ... + 1/n
+            O algoritmo deve escrever cada termo gerado e o valor final de S.
+        */
+
+        int numero;
+
+        double s = 0;
+
+        Console.WriteLine("Insira um número: ");
+        numero = int.Parse(Console.ReadLine());
+
+        for(int i = 1; i <= numero; i++){
+            s+=(1.0/i);
+            Console.WriteLine("(S) dentro do FOR: " + s);
+        }
+        Console.WriteLine("RESULTADO FINAL: " + s);
+    }
+}
