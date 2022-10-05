5f6c6b5dab216f302386889ee6c06a577e683f51
Gabriel Diniz <gabriel.assisdiniz@outlook.com>
2022-10-05 17:16:38 -0300

    Atividades 7 e 9

---
+++ Lista 2/Atividade9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program{
+
+    public static void Main(String[] args){
+
+        int total_mercadorias;
+        double preco_compra, preco_venda;
+
+        Console.WriteLine("Bem-Vindo(a)!");
+        Console.WriteLine("Insira os valores de preço de compra e venda para calcular o lucro das mercadorias!");
+
+        for(int i = 1; preco_compra > 0; i++){
+            total_mercadorias++;
+            
+            Console.WriteLine("Insira o preço de COMPRA da {0}º mercadoria: ", i);
+            preco_compra = double.parse(Console.ReadLine());
+            Console.WriteLine("Insira o preço de VENDA da {0}º mercadoria: ", i);
+            preco_venda = double.Parse(Console.ReadLine());
+
+            
+        }
+        
+
+    }
+}
