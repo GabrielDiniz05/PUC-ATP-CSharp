7d7872febe423fafa2dbd9e6dfb78dbaf7ba954c
Gabriel Diniz <gabriel.assisdiniz@outlook.com>
2022-10-06 09:59:12 -0300

    Atividade8

--- Lista 2/Atividade8/Program.cs
+++ Lista 2/Atividade8/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program{
+
+    public static void Main(String[] args){
+        int num, a = 0, fib = 1;
+
+        Console.WriteLine("Insira um número: ");
+        num = int.Parse(Console.ReadLine());
+
+
+        for(int i = 0; i < num; i++){
+            if(fib > num || a > num){
+                i = num;
+            }
+            if(fib < num || a < num){
+                Console.Write(a + " " + fib + " ");
+            
+                a+=fib;
+                fib+=a;
+               
+            }else{
+                i = num;
+            }
+        }
+    }
+}
