d8422f1d71951a5c134522121f40b13099aa859a
Gabriel Diniz <gabriel.assisdiniz@outlook.com>
2022-10-31 13:03:07 -0300

    Atividades 4 e 5

---
+++ Lista 3/Atividade04/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class Program{
+
+    static void VerifTriangulo(){
+        double c1, c2, c3;
+
+        Console.WriteLine("Insira o primeiro valor: ");
+        c1 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Insira o segundo valor: ");
+        c2 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Insira o terceiro valor: ");
+        c3 = double.Parse(Console.ReadLine());
+
+        if(c1 < (c2 + c3) && c2 < (c1+c3) && c3 < (c1+c2)){
+            if(c1 == c2 && c1 == c3 && c2 == c3){
+                Console.WriteLine("O triãngulo é equilátero!");
+            }else if(c1 == c2 || c2 == c3 || c1 == c3){
+                Console.WriteLine("O triãngulo é isósceles!");
+            }else if(c1 != c2 && c1 != c3 && c2 != c3){
+                Console.WriteLine("O triãngulo é escaleno!");
+            }
+        }else{
+            Console.WriteLine("Não é um trinângulo! Favor inserir os valores novamente: ");
+            VerifTriangulo();
+        }
+
+    }
+
+    public static void Main(string[] args){
+        bool start = true;
+        int cont = 0;
+
+
+        Console.WriteLine("Insira o comprimento dos lados do triãngulo para descobrir o tipo do mesmo!");
+
+        do{
+            if(cont < 10){
+                VerifTriangulo();
+                cont++;
+            }else{
+                string resp;
+                Console.WriteLine("Deseja verificar mais triângulos? (s/n)");
+                resp = Console.ReadLine();
+                if(resp.ToUpper() == "S"){
+                    cont = 0;
+                }else if(resp.ToUpper() == "N"){
+                    break;
+                }else{
+                    Console.WriteLine("Comando inválido! Interrompendo o programa...");
+                    break;
+                }
+            }
+        }while(start);
+    }
+}
---
+++ Lista 3/Atividade05/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Program{
+
+    static int mdc(int num1, int num2){
+        int mdc = 1, divisor = 1;
+        
+        while(num1 > 1 || num2 > 1){
+            if(num1%divisor == 0 || num2%divisor == 0){
+                int aux1 = 0, aux2 = 0;
+
+                Console.WriteLine("N1: " + num1 + "\nN2: " + num2);
+
+                if(num1 == 1){
+                    num1 = 1;
+                }
+                if(num2 == 1){
+                    num2 = 1;
+                }
+
+                aux1 = num1 / divisor;
+                aux2 = num2 / divisor;
+                
+                if(num1%divisor == 0){
+                    num1 = aux1;
+                }
+                if(num2%divisor == 0){
+                    num2 = aux2;
+                }
+                if(divisor == 1){
+                    divisor++;
+                }
+            }else{
+                divisor++;
+            }
+            if(num1%divisor == 0 && num2%divisor == 0){
+                    mdc*=divisor;
+            }
+        }
+        return mdc;
+    }
+
+    public static void Main(string[] args){
+        int n1, n2;
+
+        Console.WriteLine("Insira o primeir número: ");
+        n1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Insira o segundo número: ");
+        n2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("O MDC de {0} e {1} é: {2}", n1, n2, mdc(n1, n2));
+
+    }
+}
