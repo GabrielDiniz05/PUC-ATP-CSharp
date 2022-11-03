56e82ec6fe1e095a6713129f5947f0222c519fcb
Gabriel Diniz <gabriel.assisdiniz@gmail.com>
2022-11-02 21:49:41 -0300

    Atividade09

--- Lista 3/Atividade08/Program.cs
+++ Lista 3/Atividade08/Program.cs
@@ -5,8 +5,11 @@ class Program{
     static double calc(int num){
         double s = 0;
         for(int i = 1; i < num; i++){
-            double arg1 = (Math.Pow(i,2) + 1), arg2 = (i+3);
+            double arg1 = (Math.Pow(i,2) + 1);
+            double arg2 = (i+3);
+
             s = arg1/arg2;
+            
             // Console.WriteLine("{0} / {1} = {2}", arg1, arg2, s);
                   
         }
---
+++ Lista 3/Atividade09/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program{
+
+    static double mediaAprov(double  nota, int alunos, double notaTotal){
+        double media = 0;
+        Console.WriteLine("Número de alunos: " + alunos);
+        Console.WriteLine("Nota Total: " + notaTotal);
+        media = notaTotal / alunos;
+
+        return media;
+    }
+
+    public static void Main(string[] args){
+        bool start = true;
+        double nota = 0, media = 0, notaTotal = 0;
+        int alunos = 0, cont = 0;
+        string resp;
+
+        while(start){
+            alunos++;
+            cont++;
+            Console.WriteLine("Insira a nota do aluno: ");
+            nota = double.Parse(Console.ReadLine());
+
+            if(nota >= 6.0){
+                notaTotal+=nota;
+            }else{
+                alunos--;
+            }
+
+            media = mediaAprov(nota, alunos, notaTotal);
+
+            if(cont == 10){
+                Console.WriteLine("Deseja continuar e inserir mais notas? (s/n)");
+                resp = Console.ReadLine();
+
+                if(resp.ToUpper() == "S"){
+                    cont = 0;
+                }else if(resp.ToUpper() == "N"){
+                    break;
+                }else{
+                    Console.WriteLine("Comando inválido! Interrompendo o programa...");
+                    break;
+                }
+            }
+        }
+        Console.WriteLine("A média das notas é: " + media);
+    }
+}
