ca76159302892bdc5453ee12a4dff68aef024ae5
Gabriel Diniz <gabriel.assisdiniz@outlook.com>
2022-12-14 23:07:20 -0300

    Atividade 06

--- Lista 5/Atividade06/Program.cs
+++ Lista 5/Atividade06/Program.cs
@@ -4,9 +4,24 @@ class Program{
 
     public static void Main(string[] args){
         string caminho = @".\ler.txt";
+        int num = 0, divisor = 0;
 
         StreamWriter sw = new StreamWriter(caminho);
 
+        Console.WriteLine("Insira um número para saber os divisores: ");
+        num = int.Parse(Console.ReadLine());
         
+        
+        for(int i  = 1; i <= num; i++){
+            if(num%i == 0){
+                Console.WriteLine("Divisor:" + i);
+                divisor+=i;
+                Console.WriteLine("Soma do divisor: " + divisor);
+            }
+        }
+
+        sw.WriteLine("Soma dos divisores de "  + num  + " é: " + divisor);
+        sw.Close();
+
     }
 }
